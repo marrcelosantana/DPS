4d843680dc96b178d09ef41b5900c68dc8431ad7
marrcelosantana <marcelosantana@alu.ufc.br>
2021-10-04 20:25:42 -0300

    Initial commit

---
+++ src/Question01.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Question01 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite aqui o nome do arquivo: ");
+
+        InputStream is = new FileInputStream(scanner.nextLine());
+
+        ArrayList<String> lugares = new ArrayList<>();
+
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader bf = new BufferedReader(isr);
+
+        String line = bf.readLine();
+        lugares.add(line);
+
+        while (line != null){
+            line = bf.readLine();
+            lugares.add(line);
+        }
+
+        System.out.println("Digite a palavra que está procurando");
+        String palavra = scanner.nextLine();
+
+        for(String x: lugares){
+            if(x != null && x.contains(palavra)){
+                System.out.println(x);
+            }
+        }
+
+    }
+
+}
