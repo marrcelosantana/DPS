5b482ef154481a5161369d48b655264b96cd50d1
marrcelosantana <marcelosantana@alu.ufc.br>
2021-10-05 18:16:16 -0300

    Update +2

--- src/Question02.java
+++ src/Question02.java
@@ -1,29 +1,49 @@
-/* import java.io.*;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.Scanner;
 
-
 public class Question02 {
+    public static void Escrever(String conteudo, String destino) throws IOException{
+        OutputStream os = new FileOutputStream(destino, true);
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        if(conteudo != null){
+            bw.write(conteudo);
+            bw.newLine();
+        }
+        bw.close();
+    }
+
+    public static void Copiar(String arquivo, String destino) throws IOException{
+        InputStream is = new FileInputStream(arquivo);
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader bf = new BufferedReader(isr);
+
+        String linha = bf.readLine();
+
+        if(linha != null){
+            Escrever(linha, destino);
+        }
+
+        while(linha != null){
+            linha = bf.readLine();
+            Escrever(linha, destino);
+        }
+
+        bf.close();
+    }
+
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual o nome do seu primeiro arquivo? ");
+        String a = scanner.nextLine();
+        System.out.println("Qual o nome do seu segundo arquivo? ");
+        String b = scanner.nextLine();
+        System.out.println("Digite o nome do arquivo para onde quer copiar os dados: ");
+        String c = scanner.nextLine();
 
-        try {
-            FileWriter fw = new FileWriter("arquivo2.txt");
-            fw.write("Olá mundo, seja bem vindo ao meu código!\n");
-            fw.close();
-        } catch (Exception e) {
-            System.out.println("Erro "+e.getMessage());
-        }
-
-        try {
-            FileReader fr = new FileReader("arquivo2.txt");
-            BufferedReader br = new BufferedReader(fr);
-            String linha = br.readLine();
-            System.out.println(linha);
-
-        } catch (Exception e) {
-            System.out.println("Erro "+e.getMessage());
-        }
+        Copiar(a, c);
+        Copiar(b, c);
     }
 }
 
- */
